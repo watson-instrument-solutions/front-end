1b6502f951ceef53d1932ba5d19fbf49563e1e25
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-04 12:39:40 +1100

    create pages and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Contact from './pages/Contact';
+import Dashboard from './pages/Dashboard';
+import Confirm from './pages/Confirm';
+import Equipment from './pages/Equipment';
+import AdminPortal from './pages/AdminPortal';
+import EditBooking from './pages/EditBooking';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Container className='mb-4'>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/register' element={<Register />}/>
+      <Route path='/contact' element={<Contact />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+      <Route path='/confirm' element={<Confirm />}/>
+      <Route path='/equipment' element={<Equipment />}/>
+      <Route path='/admin-portal' element={<AdminPortal />}/>
+      <Route path='/edit-booking' element={<EditBooking />}/>
+    </Routes>
+  </Container>
+    
+
 }
 
 export default App;
---
+++ src/pages/AdminPortal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminPortal() {
+  return (
+    <div>AdminPortal</div>
+  )
+}
+
+export default AdminPortal
