a7e804d5199c1e18d03114c43f6ec4f33ce4be5c
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-05 21:09:14 +1100

    home page complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import '../Styles/home.css';
 import anechoicImg from '../docs/anechoic_unsplash.jpg';
+import { Container, Card, Row } from 'react-bootstrap';
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='home_text'>
-        <p className='home_text1'>Watson Instrument Solutions is a hire agent for 
-        sound and vibration monitoring equipment across Australia.</p>
-        <p className='home_text2'>We can deliver the tools you need when you need 
-        them, please take a look at our hire catalog for rates and availability.</p>
-        <p className='home_text3'>All our units come with NATA calibration 
-        certification, software, and associated accessories to ensure you can 
-        complete your job with confidence and meeting industry specification.</p>
-      </div>
-      <div className='home_img_container'>
-        <img src={anechoicImg} alt='home_img' className='home_img'/>
-      </div>
-    </div>
+    <Container class='position-relative' className='all_cards'>
+      <Row className='mx-2 row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2'>
+        <Card className='text mb-3 border-0'>
+          <p className='home_text1'>Watson Instrument Solutions is a hire agent for 
+          sound and vibration monitoring equipment across Australia.</p>
+          <p className='home_text2'>We can deliver the tools you need when you need 
+          them, please take a look at our hire catalog for rates and availability.</p>
+          <p className='home_text3'>All our units come with NATA calibration 
+          certification, software, and associated accessories to ensure you can 
+          complete your job with confidence to meet industry specification.</p>
+        </Card>
+        <Card className='image mb-3 border-0'>
+          <img src={anechoicImg} alt='home_img' className='home_img'/>
+        </Card>
+      </Row>
+    </Container>
   )
 }
 
