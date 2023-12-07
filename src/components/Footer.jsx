4acb62f920519e0b129a875c60c0b2aab9178643
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-07 14:13:05 +1100

    add address to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../Styles/footer.css';
 function Footer() {
   return (
     <div className='footer'>
-      <p className='address'>Address is here</p>
+      <p className='address'>65 Mattison St, Moorooka, Brisbane, QLD 4078</p>
       <p className='abn'>ABN: 0293856204956</p>
     </div>
   )
