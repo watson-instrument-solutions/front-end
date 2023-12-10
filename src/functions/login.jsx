c6c3815d375ca0d6dc5ba44d9f1dd6b490f669a7
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-10 18:07:51 +1100

    feat: login saved user to context, working OK

--- src/functions/login.jsx
+++ src/functions/login.jsx
@@ -1,23 +1,52 @@
-async function login(email, password){ 
-    console.log(email, password);
+import { useState } from "react";
+import { useUserContext } from "./useUserContext";
 
-    let result = await fetch(
-      process.env.REACT_APP_API_URL + "/users/login",
-      {
-        method: "POST", 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({email: email, password: password}),
+
+export const useLogin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { dispatch } = useUserContext();
+  
+    const login = async (email, password) => {
+      setIsLoading(true);
+      setError(null);
+      console.log(email, password);
+  
+      try {
+        let result = await fetch(
+          process.env.REACT_APP_API_URL + "/users/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+          }
+        );
+  
+        let data = await result.json();
+  
+        console.log("Response status:", result.status);
+        console.log("Response data:", data);
+  
+        if (!result.ok) {
+          setIsLoading(false);
+          setError(data.error || "Unknown error");
+        } else {
+          // save user to local storage
+          localStorage.setItem('user', JSON.stringify(data));
+  
+          // update the user context
+          dispatch({ type: 'LOGIN', payload: data });
+  
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error("Error during registration:", error);
+        setIsLoading(false);
+        setError("An unexpected error occurred during registration");
       }
-    );
-
-    let data = await result.json();
-
-    console.log(data);
-
-    return data;
-
-  }
-
-  module.exports = {login}
+    };
+  
+    return { login, isLoading, error };
+  };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,58 +1,60 @@
 import { Button, Card, Container, FormControl, InputGroup } from 'react-bootstrap'
 import '../Styles/login.css'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react';
-import { login } from '../functions/login';
+import { useState } from 'react';
+import { useLogin } from '../functions/login';
 
 function Login() {
 
-  const [jwt, setJwt] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const {login, error, isLoading} = useLogin();
 
-  // useNavigate hook from react-router for navigation
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log(`JWT value is:\n${jwt}`);
-  }, [jwt])
-
-
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    await login(email, password);
+  };
 
   return (
     <div className='login_page'>
       <Container className='login_container flex-column'>
         <h1 className='login_text'>Log in</h1>
-      <Card className='login_card border-0'>
-        <InputGroup className='mb-3 flex-column' size='lg'>
-            <FormControl style={{ margin: 'auto', width: '60%', marginBottom: '40px' }} 
-            placeholder='email'
-            type='email'
-            id='emailInput'
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-            />
-            <FormControl style={{ margin: '0 auto', width: '60%', marginBottom: '40px' }} 
-            placeholder='password'
-            type='password'
-            id='passwordInput'
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-            />
-             <div className='d-flex justify-content-center'>
-          <Button className='button border-0'style={{ backgroundColor: '#a6bcd6', color: 'white' }}
-          onClick={() => {login(email, password)}}>
-            Login
-          </Button>
-        </div>
-            <p className='register_text'>
-              Or &nbsp;<NavLink to='/register'>Register</NavLink>&nbsp; a new Account</p>
-          </InputGroup>
+        <Card className='login_card border-0'>
+          <form onSubmit={handleSubmit}>
+            <InputGroup className='mb-3 flex-column' size='lg'>
+              <FormControl
+                style={{ margin: 'auto', width: '60%', marginBottom: '40px' }}
+                placeholder='email'
+                type='email'
+                id='emailInput'
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <FormControl
+                style={{ margin: '0 auto', width: '60%', marginBottom: '40px' }}
+                placeholder='password'
+                type='password'
+                id='passwordInput'
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <div className='d-flex justify-content-center'>
+                <Button
+                  disabled={isLoading}
+                  type='submit'
+                  className='button border-0'
+                  style={{ backgroundColor: '#a6bcd6', color: 'white' }}
+                >
+                  Log in
+                </Button>
+                {error && <div className='error'>{error}</div>}
+              </div>
+            </InputGroup>
+          </form>
         </Card>
       </Container>
     </div>
-  )
+  );
 }
 
 export default Login
