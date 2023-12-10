6e4d15a062f1b013ea088553fe53925d6343ee2f
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-10 18:23:53 +1100

    feat: add conditional rendering links to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import '../Styles/navbar.css'
 import { NavLink } from 'react-router-dom';
 import { useLogout } from '../functions/logout';
+import { useUserContext } from '../functions/useUserContext';
 
 function Header() {
     const { logout } = useLogout()
+    const { user } = useUserContext()
 
     const handleClick = () => {
         logout()
@@ -20,20 +22,27 @@ function Header() {
                 <Nav.Link to='/equipment' as={NavLink}>
                     Hire Equipment
                 </Nav.Link>
+                {!user && ( <div>
                 <Nav.Link to='/login' as={NavLink}>
                     Login
                 </Nav.Link>
                 <Nav.Link to='/register' as={NavLink}>
                     Register
                 </Nav.Link>
+                </div>)}
+                {user && ( <div>
+                <Nav.Link to='/dashboard' as={NavLink}>
+                    Dashboard
+                </Nav.Link>
+                </div>)}
                 <Nav.Link to='/contact-us' as={NavLink}>
                     Contact
                 </Nav.Link>
-                {/* <div className='d-flex justify-content-center'> */}
+                { user && (<div className='d-flex justify-content-center'>
                     <Button className='border-0'
                     style={{ backgroundColor: '#a6bcd6', color: 'white' }} 
                     onClick={handleClick}>Log out</Button>
-                {/* </div> */}
+                </div>)}
             </Nav>
         </Container>
     </Navbar>
