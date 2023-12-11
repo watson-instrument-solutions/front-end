ae505e1a55255da6012b8ffb521c4fece8655ff0
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-11 20:50:23 +1100

    feat: add navigate to home on logout

--- src/functions/logout.jsx
+++ src/functions/logout.jsx
@@ -1,8 +1,10 @@
-import { useUserContext } from "./useUserContext"
+import { useUserContext } from "./useUserContext";
+import { useNavigate } from 'react-router-dom';
 
 
 export const useLogout = () => {
     const { dispatch } = useUserContext();
+    const navigate = useNavigate();
 
     const logout = () => {
         // remove user from storage
@@ -10,6 +12,10 @@ export const useLogout = () => {
 
         // dispatch logout action
         dispatch({type: 'LOGOUT'})
+
+        // navigate to home
+        console.log('logging out, returning to home page')
+        navigate('/')
     }
 
     return {logout}
