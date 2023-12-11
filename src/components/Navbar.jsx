4c18f93903e35f88ff04cfdc46db8809eca7bbe8
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-11 12:54:04 +1100

    fix: logout button displaying correctly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import '../Styles/navbar.css'
 import { NavLink } from 'react-router-dom';
 import { useLogout } from '../functions/logout';
@@ -14,37 +14,44 @@ function Header() {
     }
   return (
     <Navbar className='wisGrey mb-4'>
-        <Container className='d-flex justify-content-between'>
-            <Nav>
-                <Nav.Link to='/' as={NavLink}>
-                    Home
-                </Nav.Link>
-                <Nav.Link to='/equipment' as={NavLink}>
-                    Hire Equipment
-                </Nav.Link>
-                {!user && ( <div className='d-flex'>
-                <Nav.Link to='/login' as={NavLink}>
-                    Login
-                </Nav.Link>
-                <Nav.Link to='/register' as={NavLink}>
-                    Register
-                </Nav.Link>
-                </div>)}
-                {user && ( <div>
-                <Nav.Link to='/dashboard' as={NavLink}>
-                    Dashboard
-                </Nav.Link>
-                </div>)}
-                <Nav.Link to='/contact-us' as={NavLink}>
-                    Contact
-                </Nav.Link>
-                { user && (<div className='logout_button'>
-                    <Button className='border-0'
-                    style={{ backgroundColor: '#a6bcd6', color: 'white' }} 
-                    onClick={handleClick}>Log out</Button>
-                </div>)}
-            </Nav>
-        </Container>
+      <Container fluid>
+        <Nav>
+          <Nav.Link to='/' as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to='/equipment' as={NavLink}>
+            Hire Equipment
+          </Nav.Link>
+          {!user && (
+            <div className='d-flex'>
+              <Nav.Link to='/login' as={NavLink}>
+                Login
+              </Nav.Link>
+              <Nav.Link to='/register' as={NavLink}>
+                Register
+              </Nav.Link>
+            </div>
+          )}
+          {user && (
+            <Nav.Link to='/dashboard' as={NavLink}>
+              Dashboard
+            </Nav.Link>
+          )}
+        </Nav>
+        <div className='ml-auto'>
+          {user && (
+            <div className='logout_div'>
+              <Button
+                className='border-3'
+                style={{ backgroundColor: 'white', color: '#a6bcd6', borderColor: '#a6bcd6' }}
+                onClick={handleClick}
+              >
+                Log out
+              </Button>
+            </div>
+          )}
+        </div>
+      </Container>
     </Navbar>
   )
 }
